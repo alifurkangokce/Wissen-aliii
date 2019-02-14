3da578d58f78297b7307484cd15bc8ff156b8554
TreeGeneric <alifurkangokce@gmail.com>
2019-02-14 13:08:28 +0300

    Wissen-Form-Guncel

--- Wissen/Controllers/HomeController.cs
+++ Wissen/Controllers/HomeController.cs
@@ -44,7 +44,7 @@ namespace Wissen.Controllers
             mailMessage.Body = body;
 
             System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
-            smtp.Credentials = new System.Net.NetworkCredential("Email","Şifre");
+            smtp.Credentials = new System.Net.NetworkCredential("mail","şifre");
             smtp.EnableSsl = true;
             smtp.Send(mailMessage);
             ViewBag.Message = "Mesajınız gönderildi. Teşekkür ederiz.";
